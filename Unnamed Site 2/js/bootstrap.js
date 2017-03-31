078ef8622258f8af081ffbe3da6e846abc881118
Jasenty <ajmarriage@u.boisestate.edu>
2017-03-31 17:03:45 -0600

    now with gallery that sorts!

--- Unnamed Site 2/js/bootstrap.js
+++ Unnamed Site 2/js/bootstrap.js
@@ -2375,3 +2375,28 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+
+//Gallery View Stuff
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+
+});
